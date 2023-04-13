e0d0485ab701cd2987015e3d0d46bdce37acd5d4
DataMind <115115773+seubsons@users.noreply.github.com>
2023-04-13 01:52:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 import xarray as xr
-import numpy as np
+import pydeck as pdk
 
 infile = 'uhr_ascat_velocity.nc'
 ds = xr.open_dataset(infile)
@@ -14,6 +14,8 @@ map_center=(29,-90)
 m = leafmap.Map(center=map_center, zoom=5)
 m.add_velocity(ds, zonal_speed='u_wind', meridional_speed='v_wind')
 
-st.pydeck_chart(m.to_pydeck(height=700))
+pydeck_layer = m.pydeck_terrain()
+st.pydeck_chart(pdk.Deck(layers=[pydeck_layer], initial_view_state=pdk.ViewState(latitude=map_center[0], longitude=map_center[1], zoom=5), height=700))
+
 #m.to_streamlit(height=700)
 
