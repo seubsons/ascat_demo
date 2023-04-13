cf60660778228ed2af6d80c0a9184e1be6c9cea7
DataMind <115115773+seubsons@users.noreply.github.com>
2023-04-13 01:29:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,14 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
-#import leafmap.leafmap as leafmap
 import xarray as xr
 import numpy as np
-#import netCDF4
 
 infile = 'uhr_ascat_velocity.nc'
 ds = xr.open_dataset(infile, decode_time=False)
-#st.write(ds)
-##################################################################
-#st.set_page_config(layout="wide")
-st.set_page_config()
 
-# Customize page title
+##################################################################
+st.set_page_config(layout="wide")
+
 st.title("UHR ASCAT Coastal Winds")
 
 map_center=(29,-90)
@@ -24,5 +20,5 @@ m = leafmap.Map(center=map_center, zoom=5)
 
 #m.add_velocity(ds, zonal_speed='u_wind', meridional_speed='v_wind')
 
-#m.to_streamlit(height=700)
-m.to_streamlit()
+m.to_streamlit(height=700)
+
