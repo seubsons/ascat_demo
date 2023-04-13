dbb7be655f2e89813ca83adc842d7a67843a6b5b
DataMind <115115773+seubsons@users.noreply.github.com>
2023-04-13 02:02:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ st.set_page_config(layout="wide")
 st.title("UHR ASCAT Coastal Winds")
 map_center=(29,-90)
 m = leafmap.Map(center=map_center, zoom=5)
+m.add_basemap("HYBRID")
 m.add_velocity(ds, zonal_speed='u_wind', meridional_speed='v_wind')
 m.to_streamlit(height=700)
 
