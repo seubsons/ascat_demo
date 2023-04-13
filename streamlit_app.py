bf8f84302c457e8d7a753bd838cb527a151f5760
DataMind <115115773+seubsons@users.noreply.github.com>
2023-04-13 01:32:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ m = leafmap.Map(center=map_center, zoom=5)
 
 # m.add_basemap("HYBRID", show=False)
 # m.add_basemap("Esri.WorldStreetMap", show=False)
-# m.add_basemap('CartoDB.DarkMatter', show=False)
+m.add_basemap('CartoDB.DarkMatter', show=False)
 
 #m.add_velocity(ds, zonal_speed='u_wind', meridional_speed='v_wind')
 
